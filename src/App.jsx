a0940a837bb32b14518da6a51dfebe0a63094856
Leerish Arvind <arvindleerish@gmail.com>
2023-09-08 13:12:42 +0530

    Changed Contents

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Leerish Arvind",
+  title: "Web Developer",
+  email: "leerish.arvind@studentambassadors.com",
+  gitHub: "Leerish",
+  instagram: "_.lee_10._",
+  linkedIn: "Leerish Arvind",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
